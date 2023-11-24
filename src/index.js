a5c18e673373495c7720e00fcb154d9a1a38f203
RogerAgu <bobjackson298@gmail.com>
2023-11-24 04:54:50 -0600

    intalled express, morgan, and cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import express from 'express'
 
+import './helpers/dotenv'
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import morgan from 'morgan'
+
+const port = parseInt(process.env.PORT, 10) || 3000
+
 const app = express()
-const port = process.env.PORT || 3000
+app.use(morgan(process.env.MORGAN_LOG))
 
 app.get('/', (req, res) => {
-  res.send({ msg: 'Hello There' })
+  const title = process.env.TITLE || 'Server'
+  res.send({ msg: title })
 })
 
 app.listen(port)
